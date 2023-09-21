0b86b6b128ab558b03374e2e12b4cb1636215e34
ri-katou <hero.tra.katou@gmail.com>
2023-09-21 15:47:56 +0900

    layout.phpの作成 headタグ内はOK ヘッダー作成中

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,12 +5,17 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Layout</title>
+  <title>地元あい</title>
+  {{-- <link rel="icon" href="img/favicon.ico"> --}}
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
   <link rel="stylesheet" href="/css/styles.css">
 </head>
 
 <body>
   <header>
+    <div class="logo"><img src="/image/jimotoai-logo.png" alt="logo"></div>
+    <div><a href="register/">新規登録</a></div>
+    
   </header>
   <main>
   </main>

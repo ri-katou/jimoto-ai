267f35bc51a12f7558d5305b492236a3bcd7ef7f
ARK2053 <hello.tra.araki@gmail.com>
2023-09-21 14:12:10 +0900

    image folder

---
+++ resources/views/index.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/css/style.css">
+  <title>jimotoai</title>
+</head>
+<body>
+  <header></header>
+
+  <main>
+    <div class="page-top">
+    <div class="logo"><img src="/image/jimotoai-logo.png" alt="logo"></div>
+    <div class="logo-sub">jimoto</br>ai</div>
+    <button class="login-btn" type="button">ログイン</button>
+    <div class="yusei">
+      <p class="top-subtitle1">あなたが知ってる地元のお店やグルメ</p>
+      <p class="top-subtitle2">あなただけが知ってる地元の景色</p>
+      <p class="subtitle3">紹介してみませんか？</p>
+    </div>
+    <div class="pics">
+      <div class="top-pic-right"><img src="/image/oze.jpg" alt="oze"></div>
+      <div class="top-pic-left"><img src="/image/udon.jpg" alt="udon"></div>
+      <div class="top-pic-middle"><img src="/image/burige.jpg" alt="burige"></div>
+    </div>
+  </div>
+    <div class="top-icon">
+      <div class="mori-icon"><img src="/image/forest.svg" alt="forest"></div>
+    </div>
+    <div class="page-middle">
+    <div class="yusei">
+      <p>地元ですごい景色が撮れた！みんなにも見てほしい。</p>
+      <p>そんな地元の素晴らしさを発信できるサイトです。</p>
+    </div>
+  </div>
+    <div class="icon">
+      <div class="mori-icon-middle"><img src="" alt=""></div>
+      <div class="mori-icon-middle"></div>
+    </div>
+    <div class="page-bottom">
+    <div class="pics">
+      <div class="top-pic4"></div>
+      <div class="top-pic5"></div>
+      <div class="top-pic6"></div>
+      <div class="top-pic7"></div>
+      <div class="top-pic8"></div>
+    </div>
+    <div class="yusei">
+      <p>お店やグルメ、景色など&emsp;地元の素晴らしさは無限大！</p>
+      <p>みんなで地元の素晴らしさを伝え合おう！！</p>
+      <p class="top-kaiin">会員登録はこちら↓</p>
+    </div>
+    <button class="btn-orange" type="button">会員登録</button>
+  </div>
+  </main>
+  <hooter></hooter>
+</body>
+</html>

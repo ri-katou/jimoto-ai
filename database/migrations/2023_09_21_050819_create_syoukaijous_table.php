b9a7409cf9d83e12827ecd3db363ae7a78e6af99
“M-Satou955” <hello.tra.satou.m@gmail.com>
2023-09-21 14:50:20 +0900

    マイグレーションファイル&レイアウト作成

---
+++ database/migrations/2023_09_21_050819_create_syoukaijous_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSyoukaijousTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('syoukaijous', function (Blueprint $table) {
+            $table->unsignedInteger('id');
+            $table->integer('user_id')->unsigned();
+            $table->text('title');
+            $table->string('image1', 255);
+            $table->string('image2', 255)->nullable();
+            $table->string('image3', 255)->nullable();
+            $table->string('image4', 255)->nullable();
+            $table->text('store_infomation')->nullable();
+            $table->text('body');
+            $table->integer('municipalities_id')->unsigned();
+            $table->integer('category_id')->unsigned();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('syoukaijous');
+    }
+}

ddd5c37f0c9d096d12e41f5f9aa072e17ed8fe58
ARK2053 <hello.tra.araki@gmail.com>
2023-09-22 10:16:03 +0900

    css コメントいれたよ

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
   <link rel="stylesheet" href="/css/style.css">
-  <title>jimotoai</title>
+  <title>地元あい</title>
 </head>
 <body>
   <header></header>

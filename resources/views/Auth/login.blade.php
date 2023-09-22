2c80e5870061ebc90b70561e9478cfebf6c2e18b
“M-Satou955” <hello.tra.satou.m@gmail.com>
2023-09-22 15:43:54 +0900

    login、register要素の配置完了

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -2,23 +2,23 @@
 @section('content')
 <div class="login">
   <div class="login_body">
-    <div class="midasi">ログイン</div>
+    <div class="midasi login-header">ログイン</div>
     <div class="login_form">
       <div>
-        <label>メールアドレスを入力してください</label>
-        <input type="email" class="login_email">
+        <div>メールアドレスを入力してください</div>
+        <input type="email" class="form">
       </div>
       <div>
         <div>パスワードを入力してください</div>
-        <input type="password" class="login_pass">
+        <input type="password" class="form">
       </div>
     </div>
-    <div class="login_botan">
+    <div class="login-botan">
       <div class="return">戻る</div>
       <div>OK</div>
     </div>
   </div>
-  <div class="login_link">
+  <div class="login-link">
     <div class="link"><a href="">パスワードを忘れた場合はこちら</a></div>
     <div class="link"><a href="">新規登録はこちら</a></div>
   </div>
--- resources/views/Auth/register.blade.php
+++ resources/views/Auth/register.blade.php
@@ -1,5 +1,32 @@
 @extends('layout')
 @section('content')
+<div class="register login">
+  <div class="register_body">
+    <div class="midasi">ユーザー登録画面</div>
+    <div class="register-form">
+      <div>
+        <div>メールアドレスを入力してください</div>
+        <input type="email" class="form">
+      </div>
+      <div>
+        <div>メールアドレスを入力してください</div>
+        <input type="email" class="form">
+      </div>
+      <div>
+        <div>パスワードを入力してください</div>
+        <input type="password" class="form">
+      </div>
+      <div>
+        <div>パスワードをもう一度入力してください</div>
+        <input type="password" class="form">
+      </div>
+    </div>
+  </div>
+  <div class="login-link">
+    <div class="link"><a href="">登録済の方はこちら!</a></div>
+    <div>OK</div>
+  </div>
+</div>
 
 
 @endsection

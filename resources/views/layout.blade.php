ca2c292c35f2f2e08b547e576f633685592ea4ef
ri-katou <hero.tra.katou@gmail.com>
2023-09-22 12:16:13 +0900

    ログインページルーティング作成

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,7 +8,7 @@
   <title>地元あい</title>
   {{-- <link rel="icon" href="img/favicon.ico"> --}}
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
-  <link rel="stylesheet" href="/css/styles.css">
+  <link rel="stylesheet" href="/css/style.css">
 </head>
 
 <body>
--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,6 @@ Route::get('/', function () {
     return view('index');
 });
 Route::view('profile/','profile');
+Route::view('register/','Auth/register')
 
 

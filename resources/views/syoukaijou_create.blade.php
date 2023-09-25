c2e0fd7fed829c909d595870318fe8bc71c9fcc5
ARK2053 <hello.tra.araki@gmail.com>
2023-09-25 15:52:55 +0900

    紹介状作成　HTML完成

--- resources/views/syoukaijou_create.blade.php
+++ resources/views/syoukaijou_create.blade.php
@@ -7,45 +7,46 @@
 </div>
 <div class="syoukaijou">
 <div class="syoukaijou-title">
-  <form action="" method="post">
-  <div class="syoukaijou-title-daimei">タイトル入力（必須）</div>
+  <form action="upload.php" method="post" enctype="multipart/form-data">
+  <div class="syoukaijou-title-daimei"><div class="yusei">タイトル入力（必須）</div></div>
   <div><textarea name="title" id="" cols="80" rows="1"></textarea></div>
 </div>
 <div class="syoukaijou-main">
   <div class="syoukaijou-honbun">本文（必須）</div>
-  <div class=""><textarea name="main" id="" cols="80" rows="20"></textarea></div>
+  <div class="form"><textarea name="main" id="" cols="80" rows="20"></textarea></div>
 </div>
 <div class="syoukaijou-pic">
-  <div class="syoukaijou-pic-button">写真を追加する</div>
-  <div>※写真は必ず一枚必須</div>
-  <div class="syoukaijou-pics">
-    <div class="syoukaijou-pic1"><img src="" alt=""></div>
-    <div class="syoukaijou-pic2"><img src="" alt=""></div>
-    <div class="syoukaijou-pic3"><img src="" alt=""></div>
-    <div class="syoukaijou-pic4"><img src="" alt=""></div>
-  </div>
+    <!-- データのエンコード方式である enctype は、必ず以下のようにしなければなりません -->
+<form enctype="multipart/form-data" action="__URL__" method="POST">
+    <!-- MAX_FILE_SIZE は、必ず "file" input フィールドより前になければなりません -->
+    <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
+    <!-- input 要素の name 属性の値が、$_FILES 配列のキーになります -->
+    このファイルをアップロード: <input name="userfile" type="file" />
+    <input type="submit" value="ファイルを送信" />
+</form>
 </div>
 <div class="syoukaijou-jyanru-area">
   <div class="syoukaijou-jyanru">
     <div class="syoukaijou-jyanru-title">ジャンル</div>
-    <div class="">tag</div>
+    <select name="jyanru" id=""></select>
   </div>
-  <div class="syoukaijou-area"></div>
-  <div class="syoukaijou-area-title">エリア</div>
-  <div class="">tag</div>
+  <div class="syoukaijou-area">
+  <div class="syoukaijou-area-title">エリア</div><br><br>
+  <select name="area" id=""></select>
+  </div>
 </div>
 <div class="syoukaijou-supot">
   <div class="syoukaijou-supot-title">スポット情報</div>
   <div class="syoukaijou-supot-name">スポット名、店名（必須）</div>
-  <div class="syoukaijou-supot-name-form"><textarea name="" id="" cols="80" rows="1"></textarea></div>
+  <div class="form"><textarea name="" id="" cols="80" rows="1"></textarea></div>
   <div class="syoukaijou-supot-zyusyo">住所（任意）</div>
-  <div class="syoukaijou-supot-zyusyo-form"><textarea name="zyusyo" id="" cols="80" rows="1"></textarea></div>
+  <div class="form"><textarea name="zyusyo" id="" cols="80" rows="1"></textarea></div>
   <div class="syoukaijou-supot-url">URL（任意）</div>
-  <div class="syoukaijou-supot-url-form"><textarea name="url" id="" cols="80" rows="1"></textarea></div>
+  <div class="form"><textarea name="url" id="" cols="80" rows="1"></textarea></div>
 </div>
 <div class="syoukaijou-bottom">
   <div class="returne">戻る</div>
-  <div class="btn"><input type="submit" value="投稿する"></div>
+  <div class="syoukaijou-pic-button"><a class="btn-daidai" href=#>投稿する</a></div>
   </form>
 </div>
 </div>

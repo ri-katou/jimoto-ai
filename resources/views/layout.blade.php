70d7627a217fa309ab899d8a1b189a6cdeea80bb
ri-katou <hero.tra.katou@gmail.com>
2023-09-25 11:19:14 +0900

    ヘッダーとボタン完成

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,28 +2,31 @@
 <html lang="ja">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>地元あい</title>
-  {{-- <link rel="icon" href="img/favicon.ico"> --}}
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
-  <link rel="stylesheet" href="/css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>地元あい</title>
+    {{-- <link rel="icon" href="img/favicon.ico"> --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+    <link rel="stylesheet" href="/css/style.css">
 </head>
 
 <body>
-  <div class="rapper">
-  <header>
-    <div class="header-container">
-    <div class="logo"><img src="/image/jimotoai-logo.png" alt="logo"></div>
-    <div><a href="register/">新規登録</a></div>
-    <div class="btn-orange"><button>ログイン</button></div>
-  </div>
-  </header>
-  <main>
-    @yield('content')
-  </main>
-</div>
+    <div class="rapper">
+        <header>
+            <div class="header-container">
+                <div class="logo"><img src="/image/jimotoai-logo.png" alt="logo"></div>
+                <div class="header-not-login">
+                  <a class="btn-orange" href=#>新規登録</a>
+                  <a class="btn-green" href=#>ログイン</a>
+                </div>
+            </div>
+        </header>
+        <main>
+            @yield('content')
+        </main>
+    </div>
+    <script src="script.js"></script>
 </body>
 
 </html>

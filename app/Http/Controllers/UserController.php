0bcb7a76afa9e1433609c387c7feedb9fe474f67
ri-katou <hero.tra.katou@gmail.com>
2023-09-28 13:47:02 +0900

    ユーザーデリート完了

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -13,5 +15,8 @@ class UserController extends Controller
         $email = $request->input('email');
         $pass = $request->input('password');
         return view('auth/register_check', compact('nickname', 'email', 'pass'));
+    }
+    public function userDelete(){
+        User::find(Auth::id())->delete();
     }
 }

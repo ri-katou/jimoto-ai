cd2a4d69a9d966a5be6cebedeaba949468249035
“M-Satou955” <hello.tra.satou.m@gmail.com>
2023-09-28 11:52:59 +0900

    登録、登録確認完成

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -7,19 +7,37 @@
       @csrf
       <div class="login-form">
         <div class="form-inner">
-          <div class="login-form-top">メールアドレスを入力してください</div>
+          <div class="register-part">メールアドレスを入力してください</div>
+          @if ($errors->any())
+          <div class="error-message">
+            <ul>
+              @error('email')
+              <li>{{ $message }}</li>
+              @enderror
+            </ul>
+          </div>
+          @endif
           <input type=" email" class="form" name="email">
         </div>
         <div class="form-inner">
           <div class="login-form-top">パスワードを入力してください</div>
+          @if ($errors->any())
+          <div class="error-message">
+            <ul>
+              @error('password')
+              <li>{{ $message }}</li>
+              @enderror
+            </ul>
+          </div>
+          @endif
           <input type="password" class="form" name="password">
         </div>
       </div>
+      <div class="login-btn-space">
+        <div class="return">戻る</div>
+        <div><input type="submit" class="btn-green" value="OK"></div>
+      </div>
     </form>
-    <div class="login-btn-space">
-      <div class="return">戻る</div>
-      <div><input type="submit" class="btn-green" value="OK"></div>
-    </div>
   </div>
   <div class="login-link">
     <div class="link"><a href="">パスワードを忘れた場合はこちら</a></div>

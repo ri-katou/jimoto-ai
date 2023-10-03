06220f9143e50339f320cecd18ea874c1a1a64ca
“M-Satou955” <hello.tra.satou.m@gmail.com>
2023-10-03 14:17:25 +0900

    過去投稿一覧機能：コントローラー側のロジック完成

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\category;
+use App\Municipalitie;
 use App\Syoukaijou;
 use App\User;
 use Illuminate\Http\Request;
@@ -14,10 +16,45 @@ class HomeController extends Controller
         // ユーザー情報に紐づく紹介状情報を取得
         // $post = User::find(Auth::id())->Syoukaijous()->get();
 
-        $post = Auth::user()->Syoukaijous()->limit(3)->get();
+        $post = Auth::user()
+            ->join('Syoukaijous', 'users.id', '=', 'Syoukaijous.user_id')
+            ->join('municipalities', 'municipalities.id', '=', 'Syoukaijous.municipalities_id')
+            ->join('categories', 'categories.id', '=', 'Syoukaijous.category_id')
+            ->where('users.id', '=', Auth::user()->id)
+            ->limit(3)->get();
+
+        // $category_name = Auth::user()
+        //     ->join('Syoukaijous', 'users.id', '=', 'Syoukaijous.user_id')
+        //     ->join('categories', 'categories.id', '=', 'Syoukaijous.category_id')
+        //     ->where('users.id', '=', Auth::user()->id)
+        //     ->limit(3)->get();
+
+        // dd($post);
+
+        // $areaname = $post->
+        // $cityId = Auth::user()->Syoukaijous()->limit(3)->municipalities_id()->get();
+        // $cityName = Municipalitie::whereHas('syoukaijous', function ($name) {
+        //     //市区町村テーブルのIDと数値が同じ市区町村IDの市区町村を取得
+        //     $name->where('id',);
+        // })->get();
+
 
         return view('home', [
             'syoukai' => $post
+        ]);
+    }
+
+    public function message()
+    {
+        $message = Auth::user()
+            ->join('Syoukaijous', 'users.id', '=', 'Syoukaijous.user_id')
+            ->join('municipalities', 'municipalities.id', '=', 'Syoukaijous.municipalities_id')
+            ->join('categories', 'categories.id', '=', 'Syoukaijous.category_id')
+            ->where('users.id', '=', Auth::user()->id)
+            ->get();
+
+        return view('post_all_list', [
+            'all_message' => $message
         ]);
     }
 }

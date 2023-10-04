2d26bbe28a3511fb8b3f50b5eb72d5ce966327b4
ri-katou <hero.tra.katou@gmail.com>
2023-10-04 10:39:00 +0900

    web.phpのユーザー認証

--- routes/web.php
+++ routes/web.php
@@ -15,42 +15,51 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\EditProfile;
 
+Auth::routes();
+
 
 Route::get('/', function () {
     return view('index');
 });
-
-
-Route::get('user/area_select/', 'UserEriaContoroller@index');
-Route::post('user/area_select/', 'UserEriaContoroller@areaChoice')->name('area.select');
-
 Route::post('register/check/', 'UserController@showRegisterCheck')->name('register.check');
 
-Auth::routes();
+Route::group(['middleware' => 'auth'], function(){
+    Route::get('user/area_select/', 'UserEriaContoroller@index');
+    Route::post('user/area_select/', 'UserEriaContoroller@areaChoice')->name('area.select');
+
+    Route::get('home/', 'HomeController@test')->name('home');
+
+    Route::get('home/post/list', 'HomeController@message')->name('home.post.check');
+
+    Route::get('profile/', 'ProfileController@showProfile')->name('profile');
+
+    Route::get('profile/edit/', 'ProfileController@showProfileEdit')->name('profile.edit');
+    Route::post('profile/edit/', 'ProfileController@profileEditCheck');
+
+    Route::get('profile/edit/check', function () {
+        return view('profile_edit_check');
+    })->name('profile.edit.check');
+    Route::post('profile/edit/check', 'ProfileController@profileEditRegi');
+
+    Route::get('user/delete/', function () {
+        return view('user_delete');
+    })->name('user.delete');
+    Route::post('user/delete', 'UserController@userDelete');
+
+    Route::get('create/', 'CreateController@showCreate')->name('syoukaijou.create');
+    Route::post('create/', 'CreateController@showPreview')->name('preview.edit');
+    Route::post('create/preview/', 'CreateController@create')->name('create');
 
 
-Route::get('home/', 'HomeController@test')->name('home');
-
-Route::get('home/post/list', 'HomeController@message')->name('home.post.check');
+});
 
 
-Route::get('profile/', 'ProfileController@showProfile')->name('profile');
 
 
-Route::get('profile/edit/', 'ProfileController@showProfileEdit')->name('profile.edit');
-Route::post('profile/edit/', 'ProfileController@profileEditCheck');
-
-Route::get('profile/edit/check', function () {
-    return view('profile_edit_check');
-})->name('profile.edit.check');
-Route::post('profile/edit/check', 'ProfileController@profileEditRegi');
-
-Route::get('user/delete/', function () {
-    return view('user_delete');
-})->name('user.delete');
-Route::post('user/delete', 'UserController@userDelete');
 
 
-Route::get('create/', 'CreateController@showCreate')->name('syoukaijou.create');
-Route::post('create/', 'CreateController@showPreview')->name('preview.edit');
-Route::post('create/preview/', 'CreateController@create')->name('home');
+
+
+
+
+

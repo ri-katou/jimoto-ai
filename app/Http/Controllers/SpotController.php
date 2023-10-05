6f7eef8fe914987773eb427c7a98817f71d12f19
ri-katou <hero.tra.katou@gmail.com>
2023-10-05 23:06:27 +0900

    jsの微調整

--- app/Http/Controllers/SpotController.php
+++ app/Http/Controllers/SpotController.php
@@ -4,12 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\category;
-use App\Categorie;
 use App\Syoukaijou;
 use App\Genre;
 use App\Area;
 use App\Municipalitie;
-use Faker\Provider\sv_SE\Municipality;
+
 
 class SpotController extends Controller
 {
@@ -25,12 +24,12 @@ class SpotController extends Controller
     }
 
     public function showSpotFilter(){
-        $meisyo = Categorie::where('genre_id',3)->get();
-        $insyokuten = Categorie::where('genre_id',1)->get();
-        $gurme = Categorie::where('genre_id',2)->get();
-        $event = Categorie::where('genre_id',5)->get();
-        $shop = Categorie::where('genre_id',6)->get();
-        $onsen = Categorie::where('genre_id',4)->get();
+        $meisyo = category::where('genre_id',3)->get();
+        $insyokuten = category::where('genre_id',1)->get();
+        $gurme = category::where('genre_id',2)->get();
+        $event = category::where('genre_id',5)->get();
+        $shop = category::where('genre_id',6)->get();
+        $onsen = category::where('genre_id',4)->get();
 
         $center = Municipalitie::where('area_id',3)->get();
         $west = Municipalitie::where('area_id',4)->get();

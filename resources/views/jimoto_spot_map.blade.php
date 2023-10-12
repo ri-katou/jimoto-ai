295ff07961efea11b31df159bc89946da0a0ae74
“M-Satou955” <hello.tra.satou.m@gmail.com>
2023-10-12 16:00:45 +0900

    地図のピン、吹き出しの出し分け完了

--- resources/views/jimoto_spot_map.blade.php
+++ resources/views/jimoto_spot_map.blade.php
@@ -23,75 +23,76 @@
   </div>
   <div class="spot-map">
     <div class="spot-filter-map block-green">マップで探す</div>
-    <!-- <div id="map"></div>
-    <script>
-      function initMap() {
-        let canvas = document.getElementById('map');
-        let MyLatLng = new google.maps.LatLng(36.653141021728516, 138.84703063964844);
-        let Options = {
-          zoom: 15, //地図の縮尺値
-          center: MyLatLng, //地図の中心座標
-          mapTypeId: 'roadmap' //地図の種類
-        };
-        let map = new google.maps.Map(canvas, Options);
 
-        let marker = new google.maps.Marker({
-          map: map, //先ほど作った、地図のインスタンス([map]) を設定
-          position: MyLatLng //ピンを刺す場所 今回、ピンを挿す位置と地図の中心を同じにしています。
-        });
-      }
-    </script> -->
-    <!-- <div id="floating-panel">
-      <input id="address" type="textbox" value="群馬県">
-      <input id="submit" type="button" value="Geocode">
-    </div> -->
     <div id="map" class="spot-map"></div>
 
-    <div class="hoge">
+    <div class="address-choice" style="display: none;">
       @foreach ($all_address as $item)
-      <p>
-        {{$item->address}}
-      </p>
+      <div class="item-unique">
+        </p>
+        <p class="item-in-address">
+          {{$item->address}}
+        </p>
+        <p class="item-in-spotname">
+          {{$item->spotname}}
+        </p>
+      </div>
       @endforeach
     </div>
     <script>
       function initMap() {
         let map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 8,
+          zoom: 7,
           center: {
             lat: 36.653141021728516,
             lng: 150.644
           }
         });
         let geocoder = new google.maps.Geocoder();
-
-        // document.getElementById('submit').addEventListener('click', function() {
         geocodeAddress(geocoder, map);
-        // });
       }
 
       function geocodeAddress(geocoder, resultsMap) {
         // ここに紹介状テーブルのアドレス列の数字を一個づつ入れて行って処理を実行したい
-        let hogehoge = document.querySelectorAll('.hoge p')
-        console.log(hogehoge);
-        let address = "京都府";
-        geocoder.geocode({
-          'address': address
-        }, function(results, status) {
-          if (status === 'OK') {
-            resultsMap.setCenter(results[0].geometry.location);
-            let marker = new google.maps.Marker({
-              map: resultsMap, //先ほど作った、地図のインスタンス([map]) を設定
-              position: results[0].geometry.location //ピンを刺す場所
-            });
-          } else {
-            alert('Geocode was not successful for the following reason: ' + status);
-          }
-        });
-      }
+        let addresspin = document.querySelectorAll('.address-choice .item-in-address')
+        let infoWindows = [];
+
+        addresspin.forEach(function(element) {
+          geocoder.geocode({
+            'address': element.textContent
+          }, function(results, status) {
+            if (status === 'OK') {
+              resultsMap.setCenter(results[0].geometry.location);
+              let marker = new google.maps.Marker({
+                map: resultsMap,
+                position: results[0].geometry.location
+              });
+
+              let infoWindow = new google.maps.InfoWindow({
+                content: '<div class="sample">' + element.textContent + '</div>'
+                //複数の要素をコンテントで送る方法
+              });
+
+              marker.addListener('click', function() {
+                // 開いている infoWindow を閉じる
+                infoWindows.forEach(function(infoWin) {
+                  infoWin.close();
+                });
+
+                // クリックしたマーカーに関連する infoWindow を開く
+                infoWindow.open(map, this);
+              });
+
+              // infoWindow を配列に追加
+              infoWindows.push(infoWindow);
+            } else {
+              alert('Geocode was not successful for the following reason: ' + status);
+            }
+          });
+        })
+      };
     </script>
-    <!-- キー情報入れると警告出る -->
-    <script async defer src="https://maps.googleapis.com/maps/api/js?key=&callback=initMap">
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key={{env('GEO_API_KEY', ''),}}&callback=initMap">
     </script>
 </form>
 @endsection

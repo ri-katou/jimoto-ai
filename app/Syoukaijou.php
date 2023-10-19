1af127e23df5fb47ea275d41fd34f5241e1d0098
ARK2053 <hello.tra.araki@gmail.com>
2023-10-19 15:31:09 +0900

    キーワード　削除　修正OK

--- app/Syoukaijou.php
+++ app/Syoukaijou.php
@@ -2,9 +2,12 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
+
 
 class Syoukaijou extends Model
 {
     protected $table = 'syoukaijous';
+    use SoftDeletes;
 }

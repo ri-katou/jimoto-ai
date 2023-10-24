11c3cbcfdb235062659f7ab61a3bc9d0554e7cac
ri-katou <hero.tra.katou@gmail.com>
2023-10-25 00:11:18 +0900

    404エラー画面作成

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         // if (App::environment('production','staging')) {
-        URL::forceScheme('https');
+        // URL::forceScheme('https');
         // }
     }
 }

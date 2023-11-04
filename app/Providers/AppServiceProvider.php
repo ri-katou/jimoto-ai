b6b39338ab2f10d15e142edea9fbc1f592b25554
ri-katou <hero.tra.katou@gmail.com>
2023-11-04 18:00:17 +0900

    ローカル設定

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
     {
         // if (App::environment('production','staging')) {
 
-            URL::forceScheme('https');
+            // URL::forceScheme('https');
             // }
 
     }
